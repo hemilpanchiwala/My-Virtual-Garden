d4ab20acb98c0367d7829fcc3e7654eb575a895a
hemilpanchiwala <hpanchiwala@cs.iitr.ac.in>
2019-07-02 20:50:21 +0530

    Added the feature to water all the plants from widget on a single click

--- app/src/main/java/com/example/android/mygarden/PlantProviderWidget.java
+++ app/src/main/java/com/example/android/mygarden/PlantProviderWidget.java
@@ -5,14 +5,21 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.AppCompatDelegate;
 import android.widget.RemoteViews;
 
 import com.example.android.mygarden.ui.MainActivity;
+import com.example.android.mygarden.ui.PlantWateringService;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class PlantProviderWidget extends AppWidgetProvider {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -24,6 +31,13 @@ public class PlantProviderWidget extends AppWidgetProvider {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+
+        Intent waterIntent = new Intent(context, PlantWateringService.class);
+        waterIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        PlantWateringService.startActionWaterPlant(context);
+        PendingIntent pendingWaterIntent = PendingIntent.getActivity(context, 0, waterIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.widget_water_drop, pendingWaterIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
